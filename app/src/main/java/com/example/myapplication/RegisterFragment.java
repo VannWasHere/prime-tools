f612d300d87dfb40e3961baafa3ab1b33a742d04
VannWasHere <ateen1203@gmail.com>
2023-11-24 19:41:32 +0700

    Changing activity into fragment for ViewPager

---
+++ app/src/main/java/com/example/myapplication/RegisterFragment.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_login, container, false);
+
+        return v;
+    }
+}
