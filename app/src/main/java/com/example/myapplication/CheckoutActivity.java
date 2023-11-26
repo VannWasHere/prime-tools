021ddd3cf6e0f7018d6c7eeda6ab7de388e0a811
VannWasHere <ateen1203@gmail.com>
2023-11-26 22:01:22 +0700

    Testing Sending Data WORK

--- app/src/main/java/com/example/myapplication/CheckoutActivity.java
+++ app/src/main/java/com/example/myapplication/CheckoutActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("selectedItemID");
+        Log.d("thisisid", "The Item ID: " + id);
     }
 }
