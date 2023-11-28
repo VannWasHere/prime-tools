bbe52bddc0395cdb6a99d2d5b74bf0e180fe1051
VannWasHere <ateen1203@gmail.com>
2023-11-28 23:02:49 +0700

    Add Routing to List Order using Button Click

--- app/src/main/java/com/example/myapplication/MenuFragment.java
+++ app/src/main/java/com/example/myapplication/MenuFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MenuFragment extends Fragment {
 
+    Button menu_profile, menu_orders, menu_camera, menu_location, menu_web;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_menu, container, false);
+
+        menu_orders = v.findViewById(R.id.menu_orders);
+        menu_orders.setOnClickListener(view -> startActivity(new Intent(getActivity(), ListOrderActivity.class)));
 
         return v;
     }
