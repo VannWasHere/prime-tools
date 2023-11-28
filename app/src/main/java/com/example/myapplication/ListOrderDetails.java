eeefc7bb57633f0234565c76ef30a853898698ee
VannWasHere <ateen1203@gmail.com>
2023-11-28 23:55:31 +0700

    Add more details in ListOrderDetails

--- app/src/main/java/com/example/myapplication/ListOrderDetails.java
+++ app/src/main/java/com/example/myapplication/ListOrderDetails.java
@@ -1,7 +1,5 @@
 package com.example.myapplication;
 
-// ListOrderDetailsActivity.java
-
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +21,7 @@ import java.util.Map;
 
 public class ListOrderDetails extends AppCompatActivity {
 
-    private TextView orderDetailsTitle, itemName, orderPrice, orderAddress;
+    private TextView orderDetailsTitle, itemName, orderPrice, orderAddress, isFinished, userEmail, userPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +32,19 @@ public class ListOrderDetails extends AppCompatActivity {
         itemName = findViewById(R.id.itemName);
         orderPrice = findViewById(R.id.orderPrice);
         orderAddress = findViewById(R.id.orderAddress);
+        isFinished = findViewById(R.id.isFinished);
+        userEmail = findViewById(R.id.userEmail);
+        userPhone = findViewById(R.id.userPhone);
 
-        String order_id = getIntent().getStringExtra("order_id");
-
-        fetchOrderDetails(order_id);
+        String orderId = getIntent().getStringExtra("order_id");
+        if (orderId != null) {
+            fetchOrderDetails(orderId);
+        } else {
+            Toast.makeText(this, "Order ID not provided", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    private void fetchOrderDetails(String order_id) {
+    private void fetchOrderDetails(String orderId) {
         String url = "http://10.0.2.2:50/PrimeTools/showOrderByID.php";
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
@@ -49,6 +53,7 @@ public class ListOrderDetails extends AppCompatActivity {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
                             boolean success = jsonResponse.getBoolean("success");
+
                             if (success) {
                                 JSONObject orderDetails = jsonResponse.getJSONObject("data");
                                 displayOrderDetails(orderDetails);
@@ -70,18 +75,25 @@ public class ListOrderDetails extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
-                params.put("order_id", order_id);
+                params.put("order_id", orderId);
                 return params;
             }
         };
-
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
+
     private void displayOrderDetails(JSONObject orderDetails) throws JSONException {
         orderDetailsTitle.setText("Order Details for Order ID: " + orderDetails.getString("order_id"));
+
         itemName.setText(orderDetails.getString("item_name"));
         orderPrice.setText(orderDetails.getString("order_price"));
         orderAddress.setText(orderDetails.getString("order_address"));
+
+        int isFinishedValue = orderDetails.getInt("isFinished");
+        isFinished.setText(isFinishedValue == 1 ? "Finished" : "Not Finished");
+
+        userEmail.setText(orderDetails.getString("user_email"));
+        userPhone.setText(orderDetails.getString("user_phone"));
     }
 }
