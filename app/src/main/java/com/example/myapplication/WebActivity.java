953409e35bc6d9ce6a6d5a77c94ac92ddb9758ce
VannWasHere <ateen1203@gmail.com>
2023-12-01 00:42:13 +0700

    Add WebView (Youtube Placeholder)

--- app/src/main/java/com/example/myapplication/WebActivity.java
+++ app/src/main/java/com/example/myapplication/WebActivity.java
@@ -1,14 +1,23 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class WebActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.youtube.com/");
     }
 }
