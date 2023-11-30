479e84b18a7279b4d0b5eb7c640f85f3d290f272
VannWasHere <ateen1203@gmail.com>
2023-11-30 21:28:25 +0700

    Add MAPS Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.android.volley:volley:1.2.0")
-
+    implementation ("com.google.android.gms:play-services-maps:17.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
